8cf4ad7f83d43fce657e1a1e55f0f9e532fef504
Dinara Dulanova <dinara.dulanova@yandex.ru>
2025-07-20 23:20:36 +0300

    test

--- test/about.test.tsx
+++ test/about.test.tsx
@@ -6,6 +6,6 @@ describe("About", () => {
   it("Should render author name", () => {
     const { container } = render(<About />);
 
-    expect(container.textContent).toBe("AboutAuthor: Olga");
+    expect(container.textContent).toBe("AboutAuthor: Olya");
   });
 });
